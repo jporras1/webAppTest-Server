4933b9dd405336f5e11d103143473938186a09d3
jporras1 <javi.jr2150@gmail.com>
2019-10-23 11:00:46 -0500

    refactored weird code.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,7 +11,16 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-//
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+
+
 //    // Basic JSON "Hello, world!" example
 //    router.get("helloAgain") { req in
 //
@@ -21,10 +30,3 @@ public func routes(_ router: Router) throws {
 //        return json
 //
 //    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-}
